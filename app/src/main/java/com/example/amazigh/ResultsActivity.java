b36b3e252df0cd046d089ecc6d403142fdee3554
Tim de kok <1040606@mborijnland.nl>
2020-11-23 09:12:38 +0100

    App gekoppelt aan elkaar

---
+++ app/src/main/java/com/example/amazigh/ResultsActivity.java
@@ -0,0 +1,14 @@
+package com.example.amazigh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score);
+    }
+}
