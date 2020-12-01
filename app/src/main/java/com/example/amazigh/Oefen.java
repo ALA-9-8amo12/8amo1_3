75bf7709f796b465d63c632793f0f3b78814a34e
Nick van Slooten <nickfloor11@gmail.com>
2020-12-01 15:55:56 +0100

    Foto's V0.0000001
    Nog lang niet klaar

---
+++ app/src/main/java/com/example/amazigh/Oefen.java
@@ -0,0 +1,17 @@
+package com.example.amazigh;
+
+public class Oefen {
+
+        private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
+
+
+
--- app/src/main/java/com/example/amazigh/OefenActivity.java
+++ app/src/main/java/com/example/amazigh/OefenActivity.java
@@ -2,13 +2,30 @@ package com.example.amazigh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+//import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.bumptech.glide.Glide;
+
 
 public class OefenActivity extends AppCompatActivity {
 
+DatabaseReference mBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oefenen);
-    }
+        mBase = FirebaseDatabase.getInstance().getReference();
+        ImageView imageView = (ImageView) findViewById(R.id.FOTO);
+
+        Glide.with(this)  // this
+                .load("gs://amazigh-28582.appspot.com/Plaatjes/Dieren 01/dieren01_ezel.jpg")
+                .placeholder(R.drawable.splashscreen)
+                .into(imageView);    }
 }
